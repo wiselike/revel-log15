e4881e054c4b88654b4f01b489f1a563f5f8cdfb
Heng Wu <wuh65@chinatelecom.cn>
2025-04-16 18:02:22 +0800

    去除inconshreveable源的依赖
    Signed-off-by: Heng Wu <wuh65@chinatelecom.cn>

--- ext/ext_test.go
+++ ext/ext_test.go
@@ -2,7 +2,7 @@ package ext
 
 import (
 	"errors"
-	log "github.com/inconshreveable/log15"
+	log "github.com/wiselike/revel-log15"
 	"math"
 	"testing"
 )
--- ext/handler.go
+++ ext/handler.go
@@ -6,7 +6,7 @@ import (
 	"sync/atomic"
 	"unsafe"
 
-	log "github.com/inconshreveable/log15"
+	log "github.com/wiselike/revel-log15"
 )
 
 // EscalateErrHandler wraps another handler and passes all records through
